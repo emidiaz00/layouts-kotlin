be26aecbff32efe22d2334b5ae5bd5d687a30f09
emidiaz00 <emildsn00@gmail.com>
2022-10-19 11:05:16 -0300

    fix FragmentDashboardBinding

--- app/src/main/java/com/example/layouts/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/layouts/ui/dashboard/DashboardFragment.kt
@@ -7,11 +7,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.layouts.databinding.FragmentFrameBinding
 import com.example.layouts.databinding.FragmentLinearBinding
 
 class DashboardFragment : Fragment() {
 
-    private var _binding: FragmentLinearBinding? = null
+    private var _binding: FragmentFrameBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +23,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val frameViewModel =
+        val dashboardViewModel =
             ViewModelProvider(this).get(DashboardViewModel::class.java)
 
-        _binding = FragmentLinearBinding.inflate(inflater, container, false)
+        _binding = FragmentFrameBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        frameViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textDashboard
+        dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
